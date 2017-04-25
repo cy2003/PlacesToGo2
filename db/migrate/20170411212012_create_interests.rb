4affc81f2a70213215338e8fc8b2c8bae63a31c7
cy2003 <catyyee@hotmail.com>
2017-04-25 15:42:36 -0400

    a little styling

--- db/migrate/20170411212012_create_interests.rb
+++ db/migrate/20170411212012_create_interests.rb
@@ -3,10 +3,22 @@ class CreateInterests < ActiveRecord::Migration[5.0]
     create_table :interests do |t|
       t.integer :user_id
       t.references :interestable, polymorphic: true, index: true
-      # same as
-      # t.integer :interestable_id
-      # t.string :interestable_type
       t.timestamps
     end
   end
 end
+
+
+#same as
+
+# class CreateInterests < ActiveRecord::Migration[5.0]
+#   def change
+#     create_table :interests do |t|
+#       t.integer :user_id
+#       t.integer :interestable_id
+#       t.string :interestable_type
+#       t.timestamps
+#     end
+#     add_index :interests, [:interestable_type, :interestable_id]
+#   end
+# end
